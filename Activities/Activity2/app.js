e5a3eed526f7807c8441da9369d877a9c7a7d94e
Amelia <amelia.riding@Sofology.co.uk>
2021-02-03 09:53:36 +0000

    2 - completed querySelector activity

--- Activities/Activity2/app.js
+++ Activities/Activity2/app.js
@@ -1,24 +1,24 @@
-// const myHeading = document.querySelector("#heading");
-// const listItems = document.querySelectorAll("li:nth-child(even)");
-// // const myList = document.querySelector("#list-parent");
+const myHeading = document.querySelector("#heading");
+const listItems = document.querySelectorAll("green");
+// const myList = document.querySelector("#list-parent");
 
-// myHeading.addEventListener("click", () => {
-//     myHeading.style.color = "pink";
+myHeading.addEventListener("click", () => {
+    myHeading.style.color = "pink";
+});
+
+// myList.addEventListener("click", () => {
+//     myList.style.color = "green";
 // });
 
-// // myList.addEventListener("click", () => {
-// //     myList.style.color = "green";
-// // });
-
-// for (let listItemsIndex = 0; listItemsIndex< listItems.length; listItemsIndex ++){
-//    listItems[listItemsIndex].style.color ="lightgreen";
-// };
+for (let listItemsIndex = 0; listItemsIndex< listItems.length; listItemsIndex ++){
+   listItems[listItemsIndex].style.color ="lightgreen";
+};
 
 /*CHALLENGE*/
 
-const colors = ['red', 'orange', 'yellow', 'lightgreen', 'blue', 'purple', 'pink'];
-const listItems = document.querySelectorAll("li");
+// const colors = ['red', 'orange', 'yellow', 'lightgreen', 'blue', 'purple', 'pink'];
+// const listItems = document.querySelectorAll("li");
 
-for (let listItemsIndex = 0; listItems.length; listItemsIndex++){
-    listItems[listItemsIndex].style.color = colors[listItemsIndex];
-}
+// for (let listItemsIndex = 0; listItems.length; listItemsIndex++){
+//     listItems[listItemsIndex].style.color = colors[listItemsIndex];
+// }
