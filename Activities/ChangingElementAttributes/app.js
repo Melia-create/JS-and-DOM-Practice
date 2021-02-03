d6608b59b75d1e1dd56c816499e7fca891451272
Amelia <amelia.riding@Sofology.co.uk>
2021-02-03 11:08:44 +0000

    4 - Completed Changing Element Attributes

---
+++ Activities/ChangingElementAttributes/app.js
@@ -0,0 +1,7 @@
+const myImage = document.querySelector("#image");
+const myInput = document.querySelector("#input");
+const mySubmit = document.querySelector("#submit");
+
+mySubmit.addEventListener("click", () => {
+    myImage.src = input.value;
+})
